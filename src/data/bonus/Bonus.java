ff62a98a301196529572a47d08908ddfa65f9ae6
Ramesh Raj <slyos7@gmail.com>
2012-03-19 23:06:29 -0400

    bonus now implemented in client

---
+++ src/data/bonus/Bonus.java
@@ -0,0 +1,72 @@
+package data.bonus;
+
+import java.util.Vector;
+
+import data.me.json.JSONArray;
+import data.me.json.JSONException;
+import data.me.json.JSONObject;
+
+/**
+ * This class holds all the bonus questions
+ * @author Ramesh
+ *
+ */
+public class Bonus {
+
+	static Vector questions = new Vector();
+	
+	private static final String KEY_QUESTIONS = "questions";
+	public static final String  fileName 	  = "res/data/bonus.dat";
+	/**
+	 * DO NOT CALL THIS FUNCTION! Only used fromJSONObject or when a bonusquestion is created
+	 * @param b
+	 */
+	public static void addNewQuestion(BonusQuestion b){
+		questions.addElement(b);
+		//TODO: should sort it so get by week is quicker
+	}
+	
+	/**
+	 * Get all the questions. Probably not as useful as get by week
+	 * @return
+	 */
+	public static Vector getAllQuestions(){
+		return questions;
+	}
+	
+	/**
+	 * Get the question that was asked on a certain week
+	 * @param week 
+	 * @return a possible null BonusQuestion
+	 */
+	public static BonusQuestion getQuestionByWeek(int week){
+		for(int i=0;i<questions.size();i++){
+			BonusQuestion b = (BonusQuestion) questions.elementAt(i);
+			if(b.getWeek()==week)
+				return b;
+		}
+		return null;
+	}
+	
+	public static JSONObject toJSONObject() throws JSONException{
+		JSONObject obj = new JSONObject();
+		
+		JSONArray qA = new JSONArray();
+		
+		for(int i=0;i<questions.size();i++){
+			BonusQuestion b = (BonusQuestion) questions.elementAt(i);
+			qA.put(b.toJSONObject());
+		}
+		obj.put(KEY_QUESTIONS, qA);
+		return obj;
+	}
+	
+	public static void fromJSONObject(JSONObject o) throws JSONException{
+		JSONArray qA = o.getJSONArray(KEY_QUESTIONS);
+		for(int i =0;i<qA.length();i++){
+			BonusQuestion b = new BonusQuestion();
+			b.fromJSONObject(qA.getJSONObject(i));
+			addNewQuestion(b);
+		}
+	}
+}
